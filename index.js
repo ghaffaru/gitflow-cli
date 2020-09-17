2f159236c056357a61fe1fa8d0fc28953efe8751
Ghaffaru <mudashiruagm@gmail.com>
2020-09-17 15:55:26 +0000

    files done

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+const gitflow = require('commander');
+
 const chalk = require('chalk')
 
 const figlet = requre('figlet')
 
 const clear = require('clear');
+
+const files = require('./lib/files');
+
+gitflow
+    .command('init')
+
+    .description('Draw the app banner')
+
+    .
